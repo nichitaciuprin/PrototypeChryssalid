a1cfe16ac9b449a15abc1518e68837c95ec2cc0b
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-11 22:41:26 +0200

    UPDATE

--- Assets/_Main/Scripts/Foot.cs
+++ Assets/_Main/Scripts/Foot.cs
@@ -26,7 +26,7 @@ public class Foot : MonoBehaviour
 
             var midle = (start + end) / 2;
             midle.y += 0.3f;
-            transform.position = Helper.BezierCurve(start, midle, end, t);
+            transform.position = BezierCurve(start, midle, end, t);
 
             if (t >= 1) break;
             yield return null;
@@ -34,4 +34,8 @@ public class Foot : MonoBehaviour
 
         inProcess = false;
     }
+    private Vector3 BezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+		return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+	}
 }
--- Assets/_Main/Scripts/Helper.cs
+++ Assets/_Main/Scripts/Helper.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class Helper
 {
-    public static Vector3 BezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, float t)
-    {
-		return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
-	}
 	public static RaycastHit Raycast(Vector3 origin, Vector3 direction, float maxDistance)
     {
         RaycastHit hitInfo;

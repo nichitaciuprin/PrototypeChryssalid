7f90ee6c71c07509b949fcd38372460d5ecb1278
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-11 18:17:49 +0200

    UPDATE

--- Assets/Splines/Bezier.cs
+++ Assets/Splines/Bezier.cs
@@ -21,30 +21,4 @@ public static class Bezier
 			6f * oneMinusT * t * (p2 - p1) +
 			3f * t * t * (p3 - p2);
 	}
-	public static void Draw(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Color color)
-	{
-		var start = p0;
-		var iterations = 100;
-		for (int i = 1; i < iterations+1; i++)
-		{
-			var t = Mathf.InverseLerp(0,iterations,i);
-			var end = GetPoint(p0,p1,p2,p3,t);
-			Debug2.DrawLine(start,end,color);
-			start = end;
-		}
-	}
-	public static float SimpleDistance(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
-	{
-		var start = p0;
-		var iterations = 1000;
-		var dist = 0f;
-		for (int i = 1; i < iterations+1; i++)
-		{
-			var t = Mathf.InverseLerp(0,iterations,i);
-			var end = GetPoint(p0,p1,p2,p3,t);
-			dist += Vector3.Distance(start,end);
-			start = end;
-		}
-		return dist;
-	}
 }

b60b2a8bf12ce51607f8b2f15f3731e22776225e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-11 21:05:32 +0200

    UPDATE

--- Assets/_Main/Scripts/ColliderDraw.cs
+++ Assets/_Main/Scripts/ColliderDraw.cs
@@ -15,21 +15,18 @@ public class ColliderDraw : MonoBehaviour
         if (boxCollider == null) return;
         if (material == null) return;
 
-        if (BoxCollides(boxCollider))
-            material.color = Color.yellow;
-        else
-            material.color = Color.green;
+        material.color = BoxCollides(boxCollider) ? Color.yellow : Color.green;
     }
     private bool BoxCollides(BoxCollider box)
     {
 		var center = box.transform.position;
 		var halfExtents = box.transform.localScale / 2;
 		var rotation = box.transform.rotation;
-        var layer = ~0;
+        var layerMask = ~0;
 
         box.enabled = false;
         Physics.SyncTransforms();
-        var result = Physics.CheckBox(center,halfExtents,rotation,layer);
+        var result = Physics.CheckBox(center,halfExtents,rotation,layerMask);
         box.enabled = true;
 
         return result;
